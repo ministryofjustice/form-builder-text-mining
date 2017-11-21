5ff2c5860ff3a37b1218a55663168dea1cc0d8eb
Edward Andress <eddie_andress@hotmail.com>
2017-11-21 12:53:01 +0000

    Detect links to and from focus

--- R/build_links_json.R
+++ R/build_links_json.R
@@ -4,7 +4,14 @@ library(jsonlite)
 
 # set initial focus to one or more forms and then run everything
 # output file is called links-and-nodes.json (re-runs will overwrite )
-focus = c('pa7')
+focus = c(
+  'pa7'
+)
+
+# output file is named according to the initial focus
+output_file_name <- paste0(focus, '-vis.json')
+
+focus <- tolower(focus)
 
 links = read_csv('./links.csv')
 links$source <- gsub('\\-eng|\\-cym|\\-bil', '', links$source)
@@ -12,16 +19,23 @@ links$source <- gsub('_doc', '', links$source)
 links_to_map = tibble()
 
 get_links_to_map <- function(linksdf, forms_of_interest) {
-  filter <- linksdf$target %in% forms_of_interest
-  linksdf[filter,]
+  target_filter <- linksdf$target %in% forms_of_interest
+  source_filter <- linksdf$source %in% forms_of_interest
+  rbind(linksdf[target_filter,], linksdf[source_filter,])
 }
 
+get_new_focus <- function(new_links, prev_foci) {
+  focus_from_new_links <- c(new_links$target, new_links$source)
+  already_done <- focus_from_new_links %in% prev_foci
+  focus_from_new_links[!already_done]
+}
+
+prev_foci = c()
 while(length(focus) > 0) {
   new_links_to_map <- get_links_to_map(links, focus)
-  links_to_map <- unique(rbind(links_to_map, new_links_to_map))
-  focus <- unique(new_links_to_map$source)
-  already_done <- focus %in% links_to_map$target
-  focus <- focus[!already_done]
+  links_to_map     <- unique(rbind(links_to_map, new_links_to_map))
+  prev_foci        <- c(focus, prev_foci)
+  focus            <- get_new_focus(new_links_to_map, prev_foci)
 }
 
 nodes_to_map <- tibble(id = unique(c(links_to_map$source, links_to_map$target)))
@@ -30,6 +44,6 @@ nodes_to_map.json <- toJSON(nodes_to_map, pretty = T)
 links_to_map.json <- toJSON(links_to_map, pretty = T)
 
 jsonfile <- sprintf('{"nodes": %s, "links": %s}', nodes_to_map.json, links_to_map.json)
-write(jsonfile, './ex160-vis.json')
+write(jsonfile, output_file_name)
 
 
